89e9fa6b734e1271e872832a64f57dbc9a0714a0
Jaydeep Patel <mcpe071202@gmail.com>
2022-06-25 14:23:31 +0530

    Created pages- Admin, Login, Teacher

--- frontend/pages/admin.js
+++ frontend/pages/admin.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Admin.module.css'
 
-export default function Home() {
+export default function Admin() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="Home" />
+        <meta name="description" content="Admin" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Home
+        <h1>Admin
         </h1>
       </main>
     </div>
--- frontend/pages/teacher/[id].js
+++ frontend/pages/teacher/[id].js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Teacher.module.css'
 
-export default function Home() {
+export default function Teacher() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="Home" />
+        <meta name="description" content="Teacher" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Home
+        <h1>Teacher
         </h1>
       </main>
     </div>
